e1522623b293397c62a1160ffb111adaf726cbdf
vidimitrov <vidimi7rov@gmail.com>
2025-02-07 13:40:46 +0200

    feat: update next config and add new deps

--- next.config.js
+++ next.config.js
@@ -3,14 +3,30 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: [
-      "localhost",
-      "avatars.githubusercontent.com",
-      "lh3.googleusercontent.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
     ],
   },
-  experimental: {
-    serverActions: true,
+  env: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  },
+  // Suppress hydration warnings from Grammarly extension
+  onDemandEntries: {
+    maxInactiveAge: 25 * 1000,
+    pagesBufferLength: 2,
+  },
+  webpack: (config) => {
+    config.infrastructureLogging = {
+      level: "error",
+    };
+    return config;
   },
 };
 
