399659f61c406ab82832f02a765245d8b37626cb
vidimitrov <vidimi7rov@gmail.com>
2025-02-07 11:10:57 +0200

    fix: build error

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -156,7 +156,7 @@ export default function SignIn() {
               </button>
 
               <p className="text-center text-sm text-gray-400">
-                Don't have an account?{" "}
+                Don&apos;t have an account?{" "}
                 <Link
                   href="/auth/signup"
                   className="text-primary-400 hover:text-primary-300"
